98bb707efbea211b221411582e5e6ecef20dad75
hesterj <hesterj@ufl.edu>
2018-09-11 18:57:57 -0400

    generate terms and atomic formulas - not for = yet

--- PROVER/wffofzfc.py
+++ PROVER/wffofzfc.py
@@ -29,9 +29,13 @@ for line in siglines:
 		if (symbolinfo[1] == '0'):
 			wff.append(symbolinfo[0])
 		else:
-			predicatesymbols.append(symbolinfo)
+			if "$" not in symbolinfo[0]:
+				predicatesymbols.append(symbolinfo)
 	else:
 		connectives.append(symbolinfo)
+		
+equals = ["=","2","p"]
+predicatesymbols.append(equals)
 		
 print('connectives')
 
@@ -52,12 +56,66 @@ print('terms')
 
 for line in terms:
 	print(line)	
+
+while(len(terms)<100):
+	newterms = []
+	for funsym in functionsymbols:
+		sym = funsym[0]
+		arity = int(funsym[1])
+		termcombinations = itertools.combinations(terms, arity)
+		for acombination in termcombinations:
+			if len(newterms)>1000:
+				break
+			newterm = sym + "("
+			counter = 0
+			for term in acombination:
+				newterm += term
+				if counter != arity-1:
+					newterm += ","
+				counter += 1
+			newterm += ")"
+			if newterm not in terms:
+				newterms.append(newterm)
+	terms = terms + newterms
 	
-for funsym in functionsymbols:
-	sym = funsym[0]
-	arity = int(funsym[1])
-	termcombinations = itertools.combinations(terms, arity)
-	print(termcombinations)
+print("New terms: " + str(len(terms)))
+	
+atomicformulas = []
+
+while (len(atomicformulas)<1000):
+	
+	for predsym in predicatesymbols:
+		newatomforms = []
+		sym = predsym[0]
+		arity = int(predsym[1])
+		termcombinations = itertools.combinations(terms,arity)
+		for acombination in termcombinations:
+			
+			if len(newatomforms)>100:
+				atomicformulas = atomicformulas + newatomforms
+				break
+			
+			if sym == "=":
+				newtform = acombination[0]+"="+acombination[1]
+				if newatform not in atomicformulas:
+					newatforms.append(newatform)
+			else:
+				newatform = sym + "("
+				counter = 0
+				for term in acombination:
+					newatform += term
+					if (counter !=arity -1):
+						newatform += ","
+					counter += 1
+				newatform += ")"
+				if newatform not in atomicformulas:
+					newatomforms.append(newatform)
+		atomicformulas = atomicformulas + newatomforms
+	
+for x in atomicformulas:
+	print(x)
+	
+print(str(len(atomicformulas)))
 	
 	
 """
